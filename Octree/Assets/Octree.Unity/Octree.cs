9f1e7bbb420c2c689646d300549e4ff0645c2f82
Kristian Brodal <github.endanger041@passmail.net>
2024-11-30 12:04:31 +0100

    First working version of octrees

--- Octree/Assets/Octree.Unity/Octree.cs
+++ Octree/Assets/Octree.Unity/Octree.cs
@@ -1,19 +1,166 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Octree.Unity
 {
     public class Octree : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        class Node
         {
-        
+            private Bounds m_nodeBounds;
+            private Node[] m_children;
+            private int m_depth;
+
+            private HashSet<ISpatialData3D> m_data;
+
+            public Node(Bounds inBounds, int inDepth = 0)
+            {
+                m_nodeBounds = inBounds;
+                m_depth = inDepth;
+            }
+
+            public void AddData(Octree owner, ISpatialData3D spatialData)
+            {
+                if (m_children == null)
+                {
+                    //  Is this the first we're adding data to this node?
+                    if (m_data == null)
+                        m_data = new();
+                    
+                    //  Should we split AND are we able to split?
+                    if ((m_data.Count + 1) >= owner.PreferredMaxDataPerNode && CanSplit(owner))
+                    {
+                        SplitNode(owner);
+                        AddDataToChildren(owner, spatialData);
+                    }
+                    else
+                    {
+                        m_data.Add(spatialData);
+                    }
+                    return;
+                }
+
+                AddDataToChildren(owner, spatialData);
+            }
+
+            private void SplitNode(Octree owner)
+            {
+                //  Extents is half size of nodebounds, therefore perfect fit for subdivision.
+                Vector3 childSize = m_nodeBounds.extents;
+                Vector3 offset = childSize / 2f;
+                int newDepth = m_depth + 1;
+
+                // 8 children, the Oc in Octree.
+                m_children = new Node[8]
+                {
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3(-offset.x, -offset.y,  offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3( offset.x, -offset.y,  offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3(-offset.x, -offset.y, -offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3( offset.x, -offset.y, -offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3(-offset.x,  offset.y,  offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3( offset.x,  offset.y,  offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3(-offset.x,  offset.y, -offset.z), childSize), newDepth),
+                    new Node(new Bounds(m_nodeBounds.center + new Vector3( offset.x,  offset.y, -offset.z), childSize), newDepth),
+                };
+
+                foreach (var data3D in m_data)
+                {
+                    AddDataToChildren(owner, data3D);
+                }
+
+                m_data = null;
+            }
+
+            void AddDataToChildren(Octree owner, ISpatialData3D spatialData)
+            {
+                foreach (var child in m_children)
+                {
+                    if (child.Overlaps(spatialData.GetBounds()))
+                        child.AddData(owner, spatialData);
+                }
+            }
+
+            private bool Overlaps(Bounds other)
+            {
+                return m_nodeBounds.Intersects(other);
+            }
+
+            private bool CanSplit(Octree owner)
+            {
+                return m_nodeBounds.size.x >= owner.MinimumNodeSize &&
+                       m_nodeBounds.size.y >= owner.MinimumNodeSize &&
+                       m_nodeBounds.size.z >= owner.MinimumNodeSize;
+            }
+
+            public void FindDataInBox(Bounds searchBounds, HashSet<ISpatialData3D> foundData)
+            {
+                if (m_children == null)
+                {
+                    if (m_data == null || m_data.Count == 0)
+                        return;
+                    
+                    foundData.UnionWith(m_data);
+                    return;
+                }
+
+                foreach (var child in m_children)
+                {
+                    if (child.Overlaps(searchBounds))
+                        child.FindDataInBox(searchBounds, foundData);
+                }
+            }
+            
+            public void FindDataInRange(Vector3 searchLocation, float searchRange, HashSet<ISpatialData3D> foundData)
+            {
+                if (m_depth != 0)
+                {
+                    throw new System.InvalidOperationException(
+                        "FindDataInRange cannot be run on anything other than the root node!");
+                }
+
+                Bounds searchBounds = new Bounds(searchLocation, searchRange * Vector3.one * 2f);
+
+
+                FindDataInBox(searchBounds, foundData);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        [field: SerializeField] 
+        public int PreferredMaxDataPerNode { get; private set; } = 50;
+        [field: SerializeField] 
+        public int MinimumNodeSize { get; private set; } = 5;
         
+        private Node m_rootNode;
+        
+        public void PrepareTree(Bounds inBounds)
+        {
+            m_rootNode = new Node(inBounds);
+        }
+
+        public void AddData(ISpatialData3D data)
+        {
+            m_rootNode.AddData(this, data);
+        }
+
+        public void AddData(List<ISpatialData3D> data)
+        {
+            foreach (var data3D in data)
+            {
+                AddData(data3D);
+            }
+        }
+
+        public void ShowStats()
+        {
+            
+        }
+
+        public HashSet<ISpatialData3D> FindDataInRange(Vector3 searchLocation, float searchRange)
+        {
+            HashSet<ISpatialData3D> foundData = new();
+            m_rootNode.FindDataInRange(searchLocation, searchRange, foundData);
+            
+            return foundData;
         }
     }
 }
--- Octree/Assets/Octree.Unity/Samples/Obstacle.cs
+++ Octree/Assets/Octree.Unity/Samples/Obstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Octree.Unity.Samples
@@ -14,7 +15,34 @@ namespace Octree.Unity.Samples
         private float? m_cachedRadius;
 
         private bool m_isDirty;
-        
+
+        private Color m_oldColor;
+
+        private bool IsCachedDataDirty
+        {
+            get
+            {
+                if (m_cachedPosition == null || m_cachedBounds == null || m_cachedRadius == null)
+                    return true;
+                return !Mathf.Approximately((transform.position - m_cachedPosition.Value).sqrMagnitude, 0.0f);
+            }
+        }
+
+        private void Awake()
+        {
+            m_oldColor = m_meshRenderer.material.color = Color.red;
+        }
+
+        public void AddHighlight()
+        {
+            m_meshRenderer.material.color = Color.yellow;
+        }
+
+        public void RemoveHighlight()
+        {
+            m_meshRenderer.material.color = m_oldColor;
+        }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -29,15 +57,30 @@ namespace Octree.Unity.Samples
 
         public Vector3 GetLocation()
         {
-            
+            if (IsCachedDataDirty)
+                UpdateCachedData();
+            return m_cachedPosition.Value;
         }
 
         public Bounds GetBounds()
         {
+            if (IsCachedDataDirty)
+                UpdateCachedData();
+            return m_cachedBounds.Value;
         }
 
         public float GetRadius()
         {
+            if (IsCachedDataDirty)
+                UpdateCachedData();
+            return m_cachedRadius.Value;
+        }
+
+        private void UpdateCachedData()
+        {
+            m_cachedPosition = transform.position;
+            m_cachedBounds = m_collider.bounds;
+            m_cachedRadius = m_collider.bounds.extents.magnitude;
         }
     }
 }
